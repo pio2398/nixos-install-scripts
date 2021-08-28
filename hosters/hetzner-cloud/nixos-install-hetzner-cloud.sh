78fd62ba2fcaa555161b145e09f0067573866513
pio2398 <977500+pio2398@users.noreply.github.com>
2021-08-28 10:23:25 +0200

    Update nixos-install-hetzner-cloud.sh

--- hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
+++ hosters/hetzner-cloud/nixos-install-hetzner-cloud.sh
@@ -66,7 +66,7 @@ echo '
 
   users.users.root.openssh.authorizedKeys.keys = [
     # Replace this by your SSH pubkey!
-    "ssh-rsa AAAAAAAAAAA..."
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHdElTUPl4fAOAzTNYmLPKDeCmgT4bY2d4+9r7rLusk piotrek@pc1-nixos"
   ];
 }
 ' >> /mnt/etc/nixos/configuration.nix
